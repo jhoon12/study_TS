d26535e026d494bcbee4e51586747b7dd7051733
parkjaehoon <yukihoon12@naver.com>
2020-12-18 19:55:20 +0900

    ts useRef

--- 1217/tsreacttutorial/src/App.tsx
+++ 1217/tsreacttutorial/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import MyForm from './MyForm';
+import ReducerSample from './ReducerSample';
 
 const App: React.FC = () => {
   const onSubmit = (form: { name: string; description: string }) => {
     console.log(form);
   };
-  return <MyForm onSubmit={onSubmit} />;
+  return <ReducerSample onSubmit={onSubmit}/>;
 };
 
 export default App;
