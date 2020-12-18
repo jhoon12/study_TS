03a2606baabda4ff1e5422ca11b10f95d5434110
parkjaehoon <yukihoon12@naver.com>
2020-12-18 16:56:36 +0900

    ts reducer

--- 1217/tsreacttutorial/src/App.tsx
+++ 1217/tsreacttutorial/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Greetings from './Greetings'
-import Counter from './Counter';
+import MyForm from './MyForm';
 
 const App: React.FC = () => {
-  return (
-    <Counter/>
-  );
-}
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+  return <MyForm onSubmit={onSubmit} />;
+};
 
 export default App;
