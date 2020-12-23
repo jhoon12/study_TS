d30c27cd6422b8ed00e9987791162085f7a3893d
parkjaehoon <yukihoon12@naver.com>
2020-12-23 23:40:46 +0900

    typesafe-actions 리펙토링

--- 1217/reduxWithTS/tsredux/src/App.tsx
+++ 1217/reduxWithTS/tsredux/src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import TodoInsert from './components/TodoInsert';
-import TodoList from './components/TodoList';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <>
-      <TodoInsert />
-      <TodoList />
-    </>
+    <Counter />
   );
 }
 
