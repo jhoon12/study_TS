1eb58f2569ca890b245e413e59bbb2c03a8479df
parkjaehoon <yukihoon12@naver.com>
2020-12-28 16:13:12 +0900

    delete

--- 1216.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var a = 10;
-console.log(a);
-var color = 'sadas';
-color = 'red';
--- 1216.ts
+++
@@ -1,5 +0,0 @@
-const a: number = 10;
-console.log(a);
-
-let color : string = 'sadas';
-color = 'red'
